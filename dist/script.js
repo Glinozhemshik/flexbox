f90ffa3359800b561e85dbf9d0b32c2ea9cd494f
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-06-25 19:42:45 +0500

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,27 @@
+// var a = 123; // number
+// var b = 'dsdf'; // string
+// var c = true;  // boolean
+// var d = null;  // null
+// var e = undefined; // undefined
+// var f = {};  // object
+
+// // function myFunc 
+
+// var num = 10;
+// var str = '324';
+// // var sum = myVar + (+str);
+// console.log(typeof undefuned);
+
+const obj = {
+    foo: 'asdkfj',
+    huu: [1,2,3,"sg",{}],
+    bool: true
+}
+console.log(obj)
+
+const serialized = JSON.stringify(obj)
+console.log(serialized)
+
+const parsed = JSON.parse(serialized)
+console.log(parsed)
+`hvycvbnhХ`
